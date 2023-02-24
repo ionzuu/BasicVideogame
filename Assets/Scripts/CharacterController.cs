50395601d12b71b575a26aad8c8838f28886216d
ionzuu <jonathan.costilla@protonmail.com>
2023-02-23 21:10:39 -0600

    New Repository with basic movements

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    float dirX, dirY, moveSpeed;
+
+    Animator anim;
+
+    // Initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        moveSpeed = 5f;
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        dirY = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.position = new Vector2 (transform.position.x + dirX, transform.position.y + dirY);
+
+        /*
+
+        if (dirX != 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("Kick"))
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        if (Input.GetButtonDown("fire1") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Kick"))
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetTrigger("Hit");
+        }
+        */
+    }
+
+}
